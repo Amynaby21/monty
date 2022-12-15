2b46c7aba6e4bdc579ff3ae3b1b5bfe9f3c9e4e2
Amynaby21 <aminatandeye15@gmail.com>
2022-12-16 00:26:40 +0100

    Nop

---
+++ _nop.c
@@ -0,0 +1,14 @@
+#include "monty.c"
+
+/**
+ * _nop - Nothing to do
+ * @stack: A pointer to the top of the stack
+ * @line_number: Line number of the bycode
+ *
+ */
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
